c98f209623d4b8855cc343f82dc29d95bc57b323
ctsoni <sonitricahyono99@gmail.com>
2023-02-22 13:08:15 +0700

    add middleware for jwt validation for /users/edit endpoint 2

---
+++ auth/middleware.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"finalproject-sanber-soni/helper"
+	"finalproject-sanber-soni/service"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"net/http"
+	"strings"
+)
+
+func MiddlewareUserAuth(userService service.UserService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		var tokenString string
+		tokenArray := strings.Split(authHeader, " ")
+		if len(tokenArray) == 2 {
+			tokenString = tokenArray[1]
+		}
+
+		token, err := ValidateToken(tokenString)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		payload, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		userID := int(payload["user_id"].(float64))
+
+		user, err := userService.GetUserById(userID)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		ctx.Set("currentUser", user)
+	}
+}
